29e763198d4371c172f96b267fc9424fae695c6c
Sergio Campos <campos.srg@gmail.com>
2017-04-12 12:43:14 +0200

    add sql insert sentence

--- api.php
+++ api.php
@@ -15,7 +15,6 @@
     // retrieve the table and key from the path
     $table = array_shift($request);
     if($table === 'users'){
-        echo "TABLE: " . $table . "<br>";
         $key = array_shift($request);        
         if(empty($key)){
             echo "<br>KEY: empty<br>";
@@ -37,12 +36,11 @@
     },$input);                                    
 
     // build the SET part of the SQL command
-    // $set = '';                                                  //non-easy to read SQL sentence
+    $set = $values[0];       
     // for ($i=0;$i<count($columns);$i++) {
     //     $set.=($i>0?',':'').'`'.$columns[$i].'`=';
     //     $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
     // }
-    // echo $set."<br>";
 
     // create SQL based on HTTP method
     $result='';
@@ -52,20 +50,18 @@
         $result = $link->query($sql);                                 // You must avoid the SQL injection hack
         break;
     case 'PUT':
-        $sentence = $link->prepare("update :table set :set where id= :key");
-        $result = $sentence->execute(array(':table' => $table, ':set' => $set, ':key' => $key));
-        break;
+        // $sentence = $link->prepare("update :table set :set where id= :key");
+        // $result = $sentence->execute(array(':table' => $table, ':set' => $set, ':key' => $key));
+        // break;
     case 'POST':
-        echo "@POST works";
-        // $sql = "insert into `$table` set $set"; 
-        // $sentence = $link->prepare("insert into :table set :set");
-        // $result = $sentence->execute(array(':table' => $table, ':set' => $set));
+        $sentence = $link->prepare("insert into users (users_name) values (:set)");
+        $result = $sentence->execute(array('set' => $set));
         break;
     case 'DELETE':
-        $sql = "delete `$table` where id=$key"; 
-        $sentence = $link->prepare("delete :table where id= :key");
-        $result = $sentence->execute(array(':table' => $table, ':key' => $key));
-        break;
+        // $sql = "delete `$table` where id=$key"; 
+        // $sentence = $link->prepare("delete :table where id= :key");
+        // $result = $sentence->execute(array(':table' => $table, ':key' => $key));
+        // break;
     }
       
     // die if SQL statement failed
